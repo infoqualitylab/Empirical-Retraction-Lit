272229e78b2d3355bb19098f2dcb6599afa9a81e
Rproescholdt <57776460+Rproescholdt@users.noreply.github.com>
2020-10-11 13:52:54 -0500

    Add files via upload

--- json_pipeline.py
+++ json_pipeline.py
@@ -5,43 +5,40 @@ bibliography = json.load(infile)
 infile.close()
 
 # print(bibliography[0]["Authors"])
-print(len(bibliography))
+print("Length of bibliography: ", len(bibliography))
 
 #Make a list of all the author fields:
 author_list = []
 
-for record in range(0,(len(bibliography))): #range(0,(len(bibliography)-1?
+for record in range(0,(len(bibliography))):
   if bibliography[record]["Authors"] == "":
     author_list.append("No author listed")
   else:
     author_list.append(bibliography[record]["Authors"])
 print(author_list)
-print(len(author_list))
+print("Length of author list: ", len(author_list))
 
 #Make a new list of author fields, splitting each field where there are semicolons (turns semicolons into commas):
 new_author_list = []
 
-# for number in range(0,(len(author_list)-1)):
-#   record_index = number
-#
-
 for record in author_list:
-    new_author_list.append(record.split(";")[0].strip()))
+    if len(record.split(';')) > 0:
+        au_names = []
+        for each_au in record.split(';'):  #each_au is individual characters
+            each_au = each_au.strip()
+            if len(each_au) > 0:
+                au_names.append(each_au) #au_names is individual authors
+
+        # record = ', '.join(each_au)
+    new_author_list.append(au_names)
+
+# for ori, modified in zip(author_list, new_author_list):
+#     print('{}\n{}\n\n'.format(ori, modified))
+
+
+
 print(new_author_list)
-print(len(new_author_list))
-
-# clean_author_list = []
-
-# for record in new_author_list:
-#     if record != ' ':
-#         clean_author_list.append(record)
-# print("clean", clean_author_list)
-# print(len(clean_author_list))
-
-# for record in author_list:
-#     new_author_list.append(record.split(";")[0].strip())
-# print(new_author_list)
-# print(len(new_author_list))
+print("Length of new author list:", len(new_author_list))
 
 #Put the new author list into the author fields in the bibliography:
 for number in range(0,(len(bibliography))):
@@ -53,8 +50,8 @@ for number in range(0,len(bibliography)):
     bibliography[number]["type"] = ["Publication"]
 
 #Write out to new json file
-with open('json_output_10.8.2020.json', 'w') as json_file:
-  json.dump(bibliography, json_file)
+with open('json_output_10.11.2020.json', 'w') as json_file:
+  json.dump({"items": bibliography}, json_file)
 
 
 
