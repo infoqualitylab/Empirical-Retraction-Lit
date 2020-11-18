b18fda411980a373f6454d557f1c471ffa0b9fab
Rproescholdt <57776460+Rproescholdt@users.noreply.github.com>
2020-11-17 19:40:06 -0600

    Add files via upload

--- json_pipeline.py
+++ json_pipeline.py
@@ -1,7 +1,7 @@
 import json
 #No additional action required; library json comes with Python, and users do not need to install the library themselves
 
-infile = open('eppi_export_10.11.2020.json', 'r', encoding = 'utf-8')
+infile = open('eppi_export_11.17.2020.json', 'r', encoding = 'utf-8')
 export = json.load(infile)
 infile.close()
 
@@ -19,7 +19,7 @@ for record in range(0,(len(bibliography))):
     author_list.append("No author listed")
   else:
     author_list.append(bibliography[record]["Authors"])
-print(author_list)
+# print(author_list)
 print("Length of author list: ", len(author_list))
 
 
@@ -41,7 +41,7 @@ for record in author_list:
 #     print('{}\n{}\n\n'.format(ori, modified))
 
 
-print(new_author_list)
+# print(new_author_list)
 print("Length of new author list:", len(new_author_list))
 
 #Put the new author list into the author fields in the bibliography:
@@ -62,7 +62,7 @@ for record in bibliography:
     else:
         new_issue_list.append("(" + record["Issue"]+"),")
 
-print(new_issue_list)
+# print(new_issue_list)
 print("Length of new issue list:",len(new_issue_list))
 
 for number in range(0,(len(bibliography))):
@@ -77,13 +77,13 @@ for record in bibliography:
     else:
         new_pages_list.append(record["Pages"]+".")
 
-print(new_pages_list)
+# print(new_pages_list)
 print("Length of new pages list:",len(new_pages_list))
 
 for number in range(0,(len(bibliography))):
   bibliography[number]["Pages"] = new_pages_list[number]
 
-#Change key names to match index
+#Change key names to match index.html
 for record in bibliography:
     record["label"] = record["Title"]
     del record["Title"]
@@ -98,9 +98,51 @@ for record in bibliography:
     record["year"] = record["Year"]
     del record["Year"]
 
+#Pull out artifact data extraction codes
+for record in bibliography:
+    if "Codes" in record:
+        for item in record["Codes"]:
+            if item["AttributeId"] == 6209644:
+                record["Artifact"] = "Altmetrics to Retracted Papers"
+            if item["AttributeId"] == 5655688:
+                record["Artifact"] = "Citations to Retracted Papers"
+            if item["AttributeId"] == 5655686:
+                record["Artifact"] = "Instructions to Authors"
+            if item["AttributeId"] == 5655684:
+                record["Artifact"] = "Retraction Notices"
+            if item["AttributeId"] == 5655685:
+                record["Artifact"] = "Retracted Papers"
+            if item["AttributeId"] == 5655687:
+                record["Artifact"] = "Retraction Policies"
+
+#Pull out method data extraction codes
+for record in bibliography:
+    if "Codes" in record:
+        for item in record["Codes"]:
+            if item["AttributeId"] == 5681698:
+                record["Method"] = "Bibliometrics - Excluding Systematic Reviews"
+            if item["AttributeId"] == 5681700:
+                record["Method"] = "Citation Content Analysis"
+            if item["AttributeId"] == 5681699:
+                record["Method"] = "Content/Thematic Analysis"
+            if item["AttributeId"] == 6280798:
+                record["Method"] = "Econometrics"
+            if item["AttributeId"] == 6306062:
+                record["Method"] = "Financial Estimation"
+            if item["AttributeId"] == 5717740:
+                record["Method"] = "Formal, Systematic Review of the Literature"
+            if item["AttributeId"] == 5681702:
+                record["Method"] = "Handsearching"
+            if item["AttributeId"] == 5681701:
+                record["Method"] = "Interviews"
+            if item["AttributeId"] == 6145390:
+                record["Method"] = "Psychology Experiment"
+            if item["AttributeId"] == 5681697:
+                record["Method"] = "Survey"
+
 
 #Write out to new json file
-with open('publications.json', 'w') as json_file:
+with open('publications_11.17.2020.json', 'w') as json_file:
   json.dump({"items": bibliography}, json_file)
 
 
